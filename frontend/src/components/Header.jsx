55c88a4e86a7c73ee96f66b56c7f3ccdc5d4a829
mjennings754 <mitchellj@outlook.jp>
2026-03-22 22:27:06 +1100

    show username when logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,22 @@
 import logo from '../assets/images.png';
 import banner from '../assets/banner.png'
-import { Link } from 'react-router';
+import { Link } from 'react-router'
+import { useAuth } from '../context/AuthContext';
 function Header() {
+    const { user, logout } = useAuth(); 
     return (
     <>
     <header>
     <Link to={"/"}><img src={logo} width={100} height={100} /></Link>
     <img src={banner} width={400} />
+    {user ? (
+        <>Welcome, {user.username}</>
+    ) : (
+        <>
     <button><Link to={"/login"}>Sign in</Link></button>
     <button><Link to={"/register"}>Create an amazon account</Link></button>
+    </>
+    )}
     </header>
     </>
     )
